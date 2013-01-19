6e457a936408cb1fbdf08312bdfc4168260f8e82
Szymon Nowak <szimek@gmail.com>
2013-01-19 20:49:33 +0100

    Update ember to 1.0.0-pre4. Doesn't really work, but there are no errors.

--- js/chat/controllers/roster.js
+++ js/chat/controllers/roster.js
@@ -1,9 +1,9 @@
-Chat.Controllers.roster = Ember.ArrayProxy.create({
+Chat.Controllers.Roster = Ember.ArrayProxy.extend({
     content: [],
 
     online: function () {
         return this.filterProperty('presence', 'available');
-    }.property('@each.presence').cacheable(),
+    }.property('@each.presence'),
 
     setFriendPresence: function (presence) {
         var fullJid = presence.from,
@@ -13,9 +13,11 @@ Chat.Controllers.roster = Ember.ArrayProxy.create({
         if (friend) {
             friend.setPresence(presence);
         } else {
-            // Something went wrong. 
+            // Something went wrong.
             // Got presence notification from user not in the roster.
             console.warn('Presence update from user not in the roster: ' + fullJid + ':' + presence.type);
         }
     }
 });
+
+Chat.Controllers.roster = Chat.Controllers.Roster.create();
